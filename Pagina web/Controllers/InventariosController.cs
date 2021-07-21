a08bc7f5f117ef93b9d4453ba41b92ea2f904e53
zahid ce <zahidce1@hotmail.com>
2021-07-21 14:22:39 -0500

    Cambio Startuo

---
+++ Pagina web/Controllers/InventariosController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Pagina_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pagina_web.Controllers
+{
+    public class InventariosController : Controller
+    {
+        // GET: InventariosController
+        public ActionResult Index()
+        {
+            List<Inventario> ltsinventario = new List<Inventario>();
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Listapersonas")))
+            {
+                Inventario inv = new Inventario();
+                inv.Nombre = "Zahid";
+                inv.Apellido = "Cepeda";
+                inv.Cedula = "1756624799";
+                inv.Direccion = "Magdalena";
+                inv.Genero = "Masculino";
+                for (int i= 0; i < 10; i++)
+                {
+                ltsinventario.Add(inv);
+                }
+            }
+            else
+            {
+                ltsinventario = JsonConvert.DeserializeObject<List<Inventario>>(HttpContext.Session.GetString("Listapersonas"));
+            }
+            HttpContext.Session.SetString("Listapersonas", JsonConvert.SerializeObject(ltsinventario));
+               
+            return View(ltsinventario);
+        }
+
+        // GET: InventariosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: InventariosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InventariosController/Create
+        [HttpPost]  
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Inventario inventario)
+        {
+            try
+            {
+                List<Inventario> inven = new List<Inventario>();
+                inven = JsonConvert.DeserializeObject<List<Inventario>>(HttpContext.Session.GetString("Listapersonas"));
+                inven.Add(inventario);
+                HttpContext.Session.SetString("Listapersonas", JsonConvert.SerializeObject(inven));
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventariosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: InventariosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: InventariosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Pagina web/Models/Inventario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pagina_web.Models
+{
+    public class Inventario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Genero { get; set; }
+    }
+}
